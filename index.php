86be9d57048420ff95ad1f99421963e7bbcfc7cb
Tim Eckel <teckel@sscorp.com>
2017-08-25 12:00:01 -0400

    Remove Bootstrap and setup simple Vue framework

--- index.php
+++ index.php
@@ -1,35 +1,63 @@
 <!DOCTYPE html>
 <html lang="en-US" class="no-js">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<meta name="description" content="">
-	<meta name="keywords" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-	<link href="/css/bootstrap_custom.css" rel="stylesheet" type="text/css">
-	<link href="/css/style.css" rel="stylesheet" type="text/css">
-	<link href="/fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+		<link href="/css/bootstrap_custom.css" rel="stylesheet" type="text/css">
+		<link href="/css/style.css" rel="stylesheet" type="text/css">
+		<link href="/fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+	</head>
 
-<body class="css-transitions-off waiting">
-	<!--[if lt IE 8]>
-	<p class="viewerror">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.</p>
-	<![endif]-->
+	<body class="css-transitions-off waiting">
+		<script type='text/javascript'>//<![CDATA[
+		document.write("<script async src='//HOST:3000/browser-sync-client.1.1.2.js'><\/script>".replace(/HOST/g, location.hostname));
+		//]]></script>
+		<!--[if lt IE 8]>
+		<p class="viewerror">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
 
-	<div id="header">
-	</div>
+		<div id="header">
+		    <div class="contain">
+                <h1>Title</h1>
+			</div>
+		</div>
 
-	<div id="main">
-	</div>
+		<div id="main">
+			<p>Hello world!</p>
+			<div id="vue">
+				<ul>
+					<li v-for="(item, index) in items">
+						{{ item.message }}
+					</li>
+				</ul>
+			</div>
+		</div>
 
-	<div id="footer">
-	</div>
+		<div id="footer">
+		</div>
 
-	<script src="/js/jquery-3.0.0.min.js" type="text/javascript"></script>
-	<script src="/js/vue-2.0.1.min.js"></script>
-	<script src="/js/jquery.tablesorter.min-2.26.6.js" type="text/javascript"></script>
-	<script src="/js/jquery.tablesorter.widget-filter.min-2.26.6.js" type="text/javascript"></script>
-	<script src="/scripts/custom.js" type="text/javascript"></script>
-</body>
+		<div id="popupModal">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-body">
+						<div class="modal-copy"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="waiting"><i class="fa fa-spinner fa-spin fa-fw"></i></div>
+		<div id="toast"></div>
+
+		<script src="/js/jquery-3.0.0.min.js" type="text/javascript"></script>
+		<script src="/js/vue-2.0.1.min.js"></script>
+<!--
+		<script src="/js/jquery.tablesorter.min-2.26.6.js" type="text/javascript"></script>
+		<script src="/js/jquery.tablesorter.widget-filter.min-2.26.6.js" type="text/javascript"></script>
+-->
+		<script src="/js/custom.js" type="text/javascript"></script>
+	</body>
 </html>
--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,76 @@
+// Create the Vue
+vm = new Vue({
+	el: '#vue',
+	data: {
+		items: [
+			{ message: 'Foo' },
+			{ message: 'Bar' }
+		]
+	}
+});
+
 $(document).ready(function(){
 	/* Turns CSS transitions back on once page is ready */
 	$("body").removeClass("css-transitions-off");
-	
+
+	// Don't cache AJAX
+	$.ajaxSetup({ cache: false });
+
 	/* Remove indicator that JavaScript is disabled */
 	$("html").removeClass("no-js");
+
+	/* Remove waiting animation */
+	waitEnd();
 });
+
+function popupModal(content, confirm = false, callback) {
+	waitEnd();
+	$('body').addClass('popup');
+	$('#popupModal .modal-copy').html(content);
+	$('#popupModal').fadeIn(200);
+    if (confirm == 'okay')
+	    $('#popupModal .modal-copy').append('<div class="confirm-buttons"><button class="okay"><i class="fa fa-check"></i> Okay</button></div>');
+    else if (confirm == true)
+	    $('#popupModal .modal-copy').append('<div class="confirm-buttons"><button class="okay"><i class="fa fa-check"></i> Yes</button><button class="cancel"><i class="fa fa-times"></i> No</button></div>');
+    else
+	    $('#popupModal .modal-copy').append('<div class="confirm-buttons center"><button class="okay"><i class="fa fa-exclamation-triangle"></i> Okay</button></div>');        
+    $('#popupModal .confirm-buttons button').click(function(event) {
+        $('#popupModal').fadeOut(200);
+        $('body').removeClass('popup');
+        $('.modal-body').scrollTop(0);
+        if (confirm == true || confirm == 'okay')
+            callback($(event.target).closest('button').hasClass('okay'));
+    });
+}
+
+function glutenFreeToast(message, special) {
+	$('#toast').append('<div><i class="fa"></i><span>' + message + '<span></div>');
+    var newToast = $('#toast').find('div:last');
+	if (special == 'Occupied')
+		$(newToast).addClass('occupied');
+	else if (special == 'Available')
+		$(newToast).addClass('available');
+	else if (special == 'Error')
+		$(newToast).addClass('error');
+    var newToastCancel = $(newToast).find('i.fa');
+    setTimeout(function() {
+        $(newToast).addClass('show');
+    }, 100);
+    $(newToastCancel).click(function() {
+        $(newToast).removeClass('show');
+        setTimeout(function() {
+            $(newToast).remove();
+        }, 200);
+    });
+    setTimeout(function() {
+        $(newToastCancel).trigger('click');
+    }, 8000);
+}
+
+function waitStart() {
+	$('body').addClass('waiting');
+}
+
+function waitEnd() {
+	$('body').removeClass('waiting');
+}
